66de1dca1a7dc2ed53dd8a5791bf76b70048ea92
Rogerio-kayronny-sales <108687095+Kayronny123@users.noreply.github.com>
2022-11-22 20:04:40 -0300

    Initial commit

---
+++ src/components/CardGrande/CardGrande.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  LayoutCardGrande,
+  ImgCardGrande,
+  InfosCard,
+  TituloCardGrande
+} from "./styles";
+
+function CardGrande(props) {
+  return (
+    <LayoutCardGrande>
+      <ImgCardGrande src={props.imagem} />
+      <InfosCard>
+        <TituloCardGrande>{props.nome}</TituloCardGrande>
+        <p>{props.descricao}</p>
+      </InfosCard>
+    </LayoutCardGrande>
+  );
+}
+export default CardGrande;
